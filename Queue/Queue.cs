d0c250da16c5ae7aa213a697c419349ab02fe17c
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-11-18 11:36:23 +0300

    `feat:` add dummy `Queue`

---
+++ Queue/Queue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Queue
+{
+    /// <summary>
+    /// Represents the queueing model.
+    /// </summary>
+    public class Queue
+    {
+        private Byte a;
+        private Byte b;
+        private Byte c;
+        private Byte d;
+
+        public Double First { get; set; }
+        public Double Second { get; set; }
+
+        public Queue()
+        {
+            First = 0.5;
+            Second = 0.5;
+
+            ClearState();
+        }
+
+        public Queue(Double first, Double second)
+        {
+            First = first;
+            Second = second;
+
+            ClearState();
+        }
+
+        public void ClearState()
+        {
+            a = 2;
+            b = 0;
+            c = 0;
+            d = 0;
+        }
+    }
+}
