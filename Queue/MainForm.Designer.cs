a6248e2eaeea2facbb5996d6db84ce130cb4c99c
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-11-18 11:04:49 +0300

    `feat:` add dividers

--- Queue/MainForm.Designer.cs
+++ Queue/MainForm.Designer.cs
@@ -7,9 +7,17 @@ namespace Queue
     {
         private Button button;
 
+        private Label divider1;
+        private Label divider2;
+        private Label divider3;
+
         private void InitializeComponent()
         {
             button = new Button();
+
+            divider1 = new Label();
+            divider2 = new Label();
+            divider3 = new Label();
 
             SuspendLayout();
 
@@ -20,6 +28,36 @@ namespace Queue
             button.Name = "button";
             button.Size = new Size(590, 30);
             button.Text = "Generate";
+
+            #endregion
+
+            #region divider1
+
+            divider1.AutoSize = false;
+            divider1.BorderStyle = BorderStyle.Fixed3D;
+            divider1.Location = new Point(5, 75);
+            divider1.Name = "divider1";
+            divider1.Size = new Size(590, 2);
+
+            #endregion
+
+            #region divider2
+
+            divider2.AutoSize = false;
+            divider2.BorderStyle = BorderStyle.Fixed3D;
+            divider2.Location = new Point(5, 152);
+            divider2.Name = "divider2";
+            divider2.Size = new Size(590, 2);
+
+            #endregion
+
+            #region divider3
+
+            divider3.AutoSize = false;
+            divider3.BorderStyle = BorderStyle.Fixed3D;
+            divider3.Location = new Point(5, 358);
+            divider3.Name = "divider3";
+            divider3.Size = new Size(590, 2);
 
             #endregion
 
@@ -34,7 +72,10 @@ namespace Queue
 
             Controls.AddRange(new Control[]
             {
-                button
+                button,
+                divider1,
+                divider2,
+                divider3,
             });
 
             #endregion
