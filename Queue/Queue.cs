9351c197fe9368bdd216a1df894e59423431a463
Anatoly Nadolsky <a.nadolsky@proton.me>
2023-11-18 23:34:08 +0300

    `feat!:` implement `Queue` class

--- Queue/Queue.cs
+++ Queue/Queue.cs
@@ -7,6 +7,8 @@ namespace Queue
     /// </summary>
     public class Queue
     {
+        private readonly Random random = new Random();
+
         private Byte a;
         private Byte b;
         private Byte c;
@@ -14,6 +16,10 @@ namespace Queue
 
         public Double First { get; set; }
         public Double Second { get; set; }
+
+        public Int32 RequestCount { get; private set; }
+        public Int32 FailureCount { get; private set; }
+        public Int32 QueueCount { get; private set; }
 
         public Queue(Double first, Double second)
         {
@@ -24,6 +30,80 @@ namespace Queue
 
             First = first;
             Second = second;
+
+            RequestCount = 0;
+            FailureCount = 0;
+            QueueCount = 0;
+        }
+
+        public void NextState()
+        {
+            if (d == 1)
+            {
+                if (random.NextDouble() > First)
+                {
+                    d = 0;
+                }
+            }
+
+            if (c == 1)
+            {
+                if (d == 0)
+                {
+                    c = 0;
+                    d = 1;
+                }
+            }
+
+            if (b == 1)
+            {
+                if (random.NextDouble() > Second)
+                {
+                    if (c == 0)
+                    {
+                        c = 1;
+
+                        if (d == 0)
+                        {
+                            c = 0;
+                            d = 1;
+                        }
+                        else
+                        {
+                            QueueCount++;
+                        }
+                    }
+                    else
+                    {
+                        FailureCount++;
+                    }
+
+                    b = 0;
+                }
+            }
+
+            if (a == 1)
+            {
+                a = 2;
+            }
+            else
+            {
+                a = 1;
+            }
+
+            if (a == 2)
+            {
+                RequestCount++;
+
+                if (b == 0)
+                {
+                    b = 1;
+                }
+                else
+                {
+                    FailureCount++;
+                }
+            }
         }
     }
 }
